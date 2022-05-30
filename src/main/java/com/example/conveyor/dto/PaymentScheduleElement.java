a01f29fd42e7369f2140e60fc55ed3c3569fa02e
Зотов Александр <alzotov@neoflex.ru>
2022-05-30 12:15:37 +0300

    Changed validation and payment schedule methods

--- src/main/java/com/example/conveyor/dto/PaymentScheduleElement.java
+++ src/main/java/com/example/conveyor/dto/PaymentScheduleElement.java
@@ -2,6 +2,7 @@ package com.example.conveyor.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Schema(description = "График платежей")
